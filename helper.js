66b5553b8e2bc40389d286c2f761e8c773c3fb0e
Brandon Tran <tranbrandon773@gmail.com>
2024-06-05 15:46:08 -0700

    logic for getting files changed from PR

--- helper.js
+++ helper.js
@@ -10,7 +10,7 @@ export async function getWorkflowLogs(octokit, owner, repo, runId) {
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
-            })
+            });
         logsUrl = res.url;
         // logs = await axios.get(logsUrl, {
         //     responseType: 'arraybuffer',  
@@ -25,8 +25,25 @@ export async function getWorkflowLogs(octokit, owner, repo, runId) {
     console.log(`Logs URL: ${logsUrl}`);
 };
 
-export async function getFilesFromPullRequest() {
-
+export async function getFilesChangedFromPullRequest({octokit, payload}) {
+    let filesChanged;
+    try {
+        const res = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+            owner: payload.repository.owner.login,
+            repo: payload.repository.name,
+            pull_number: payload.pull_requests[0].number,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+            });
+        filesChanged = res
+    } catch (error) {
+        if (error.response) { 
+            console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+        }
+        console.error(error);
+    }
+    console.log(res)
 }
  
 export async function getFileContent(octokit, owner, repo, path, sha) {
@@ -39,7 +56,7 @@ export async function getFileContent(octokit, owner, repo, path, sha) {
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
-            })
+            });
     } catch (error) {
         if (error.response) { 
             console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
