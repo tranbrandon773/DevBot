3b0ba03d8e4a08457835df0b05124020ddf7b9cc
Brandon Tran <tranbrandon773@gmail.com>
2024-06-06 18:06:26 -0700

    Remove get files changed from PR b/c probably unnecessary

--- helper.js
+++ helper.js
@@ -45,27 +45,6 @@ export function findFilesFromErrors(errors) {
     return files;
 }
 
-export async function getFilesChangedFromPullRequest(octokit, payload) {
-    let filesChanged;
-    try {
-        const res = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
-            owner: payload.repository.owner.login,
-            repo: payload.repository.name,
-            pull_number: payload.workflow_run.pull_requests[0].number,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-            });
-        filesChanged = res.data
-    } catch (error) {
-        if (error.response) { 
-            console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
-        }
-        console.error(error);
-    }
-    console.log(filesChanged)
-}
- 
 export async function getFileContent(octokit, owner, repo, path, sha) {
     try {
         await octokit.request('GET /repos/{owner}/{repo}/contents/{path}?ref={ref}', {
