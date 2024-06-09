f6feae425d3eb3ae0f57d50b1f808eb968b7f512
pvvang <cs.wang@proton.me>
2024-06-08 20:28:22 -0700

    delete line used for debugging

--- mock.sh
+++ mock.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
+
 unique_name=${2//[^[:alnum:]_]/}
 zipname="${unique_name}.zip"
 mkdir "$unique_name"
 curl $1 -L -o "${unique_name}/${zipname}"
 echo "${unique_name}/${zipname}"
 unzip "${unique_name}/${zipname}" -d "$unique_name"
-cat "${unique_name}/0_build.txt"
