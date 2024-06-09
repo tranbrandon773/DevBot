acca23042a67a6d28acb95e6cd932f35bb3b3348
Brandon Tran <tranbrandon773@gmail.com>
2024-06-08 18:49:06 -0700

    inferencing with openai

--- openai.js
+++ openai.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const key = process.env.OPENAI_API_KEY
-gi
+
 const openai = new OpenAI({
     apiKey: key
 });
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "Below is an instruction that describes a task. Write a response that appropriately completes the request. You are given the old and new code of a given file along with the errors that occurred in the new code. Please fix the errors. Only output the code."},
+    messages: [{ role: "system", content: "Attached is an instruction that describes a task. Write a response that appropriately completes the request. You are given the old and new code of a given file along with the errors that occurred in the new code. Please fix the errors. Only output the code."},
                 {role: "user", content: "Old Code: ```print('Hello World!')``` New Code: ```import numpy as np \n print('Hello World!')``` Errors: [main.py:1:17: F401 `numpy` imported but unused]"}
     ],
     model: "gpt-4o",
