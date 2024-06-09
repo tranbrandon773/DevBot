81f6007d4ed9d7a46a90471ebabc08a721d1a3fc
pvvang <cs.wang@proton.me>
2024-06-08 19:30:07 -0700

    minor test on shell

--- mock.sh
+++ mock.sh
@@ -1,10 +1,9 @@
 unique_name=${2//[^[:alnum:]_]/}
 zipname="${unique_name}.zip"
 mkdir "$unique_name"
-cd "$unique_name"
-curl -LO $1
-echo $zipname
-mv signedlogcontent* $zipname
-unzip $zipname
-rm $zipname
-cd ..
+curl -LO $1 --output "$unique_name"
+echo "${unique_name}/signedlogcontent*"
+echo "${unique_name}/${zipname}"
+# mv "${unique_name}/signedlogcontent*" "${unique_name}/${zipname}"
+# unzip $zipname
+# rm $zipname
