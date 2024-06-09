0f192f228e056808af6e2c40f07ed2cb071bd45e
Brandon Tran <tranbrandon773@gmail.com>
2024-06-08 21:34:22 -0700

    remove directory before making directory

--- mock.sh
+++ mock.sh
@@ -2,6 +2,7 @@
 
 unique_name=${2//[^[:alnum:]_]/}
 zipname="${unique_name}.zip"
+rm -rf "$unique_name"
 mkdir "$unique_name"
 curl $1 -L -o "${unique_name}/${zipname}"
 echo "${unique_name}/${zipname}"
